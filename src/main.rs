5980fd61f333a37736de18c2531708f3b5e68013
J. David Giese <johndgiese@gmail.com>
2016-10-07 09:59:02 -0400

    Read in the first argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+
 fn main() {
-    println!("Hello world!");
+    let mut args = env::args();
+
+    match args.nth(1) {
+        None => {
+            println!("Usage: fuzzy PATTERN [FILE...]");
+            std::process::exit(1);
+        },
+        Some(pattern) => {
+            println!("pattern = {}", pattern);
+        }
+    }
+    // TODO: everything
 }
